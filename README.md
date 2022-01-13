930f38e3598950538ba7eb136781d3163fd86f1d
Alistair Johnston <2560836j@student.gla.ac.uk>
2022-01-13 17:09:00 +0000

    Chapter 3 - no excercises

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tango_with_django_project
+Project following the 'Tango with Django 2' book.
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name = 'index'),
+
+    path('rango/', include('rango.urls')), #designate URLs starting with rango to rango
+
     path('admin/', admin.site.urls),
 ]
